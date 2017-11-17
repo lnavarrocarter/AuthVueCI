68d5ceb6843fade0b02bbf306b582076d3acbb43
luis navarro <luis@youtouch.cl>
2017-11-17 09:50:47 -0300

    commit

--- vue/webpack.config.js
+++ vue/webpack.config.js
@@ -30,7 +30,17 @@ module.exports = {
         options: {
           name: '[name].[ext]?[hash]'
         }
-      }
+      },
+	  {
+		test: /\.scss$/,
+		use: [{
+			loader: "style-loader" // creates style nodes from JS strings
+		}, {
+			loader: "css-loader" // translates CSS into CommonJS
+		}, {
+			loader: "sass-loader" // compiles Sass to CSS
+		}]
+	  }
     ]
   },
   resolve: {
